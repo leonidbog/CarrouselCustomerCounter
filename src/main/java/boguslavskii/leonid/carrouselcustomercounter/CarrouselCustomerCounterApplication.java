ff374559ae965a6da79acaa58564c647423d4d37
leonidboguslavskii <le.boguslavskii@gmail.com>
2025-02-06 14:48:18 +0200

    init commit

---
+++ src/main/java/boguslavskii/leonid/carrouselcustomercounter/CarrouselCustomerCounterApplication.java
@@ -0,0 +1,13 @@
+package boguslavskii.leonid.carrouselcustomercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrouselCustomerCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrouselCustomerCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boguslavskii/leonid/carrouselcustomercounter/CarrouselCustomerCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package boguslavskii.leonid.carrouselcustomercounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrouselCustomerCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
