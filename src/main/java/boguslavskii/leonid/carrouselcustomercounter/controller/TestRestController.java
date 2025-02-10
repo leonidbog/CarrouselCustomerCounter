9f95aa0dfc380643379617eab1ca66691584b50b
leonidboguslavskii <le.boguslavskii@gmail.com>
2025-02-10 14:22:24 +0200

    add docker compose config

---
+++ src/main/java/boguslavskii/leonid/carrouselcustomercounter/controller/TestRestController.java
@@ -0,0 +1,15 @@
+package boguslavskii.leonid.carrouselcustomercounter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestRestController {
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
