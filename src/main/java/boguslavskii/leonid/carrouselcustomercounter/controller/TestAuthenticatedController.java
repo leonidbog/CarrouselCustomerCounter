46ebefda091f2185d327a3d0fa5e23b9aea82ffc
leonidboguslavskii <le.boguslavskii@gmail.com>
2025-02-14 15:47:41 +0200

    added jwt authentication logic

--- src/main/java/boguslavskii/leonid/carrouselcustomercounter/controller/TestAuthenticatedController.java
+++ src/main/java/boguslavskii/leonid/carrouselcustomercounter/controller/TestAuthenticatedController.java
@@ -1,5 +1,6 @@
 package boguslavskii.leonid.carrouselcustomercounter.controller;
 
+import boguslavskii.leonid.carrouselcustomercounter.dto.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,14 @@ public class TestAuthenticatedController {
     @GetMapping("hello")
     public String helloWorld() {
         return "Hello authenticated LEONID and DMITRY!";
+    }
+
+    @GetMapping("login")
+    public Response login() {
+        Response response = new Response();
+        response.setStatusCode(200);
+        response.setMessage("Success");
+        return response;
     }
 
 
